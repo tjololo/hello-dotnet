81dccb62395b6e3184d9e5e24048993192826763
Vemund Gaukstad <tjololo@users.noreply.github.com>
2021-02-09 11:49:32 +0100

    remove unused using

--- Controllers/HelloController.cs
+++ Controllers/HelloController.cs
@@ -1,7 +1,6 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Configuration;
 using System.Net.Http;
 using hello_dotnet.Downstream;
 
